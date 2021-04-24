184cef9ee210ad3da6e00bc1bc05e2916919bb02
Taylor Cooke <taylorcooke@gmail.com>
2021-04-24 12:38:51 -0400

    added deck and player class

--- cards.py
+++ cards.py
@@ -44,3 +44,24 @@ class Card:
             self.suits[self.suit]
         return v
 
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards\
+                    .append(Card(i,
+                                 j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
+
+
+class Player:
+    def __init__(self, name):
+        self.wins = 0
+        self.card = None
+        self.name = name
